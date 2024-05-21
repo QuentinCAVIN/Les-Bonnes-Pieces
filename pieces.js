09251215939426516a6ed65f4aec0f8db65f424e
Quentin CAVIN <quentin.cavin@gmail.com>
2024-05-21 18:36:13 +0200

    Exercice P1C4

--- pieces.js
+++ pieces.js
@@ -18,9 +18,16 @@ prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" 
 const categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
 
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = `${article.description ?? "Pas de description pour le moment"}`;
+
+const disponibiliteElement = document.createElement("p");
+disponibiliteElement.innerText = article.disponibilité ? "En stock" : "Rupture de stock";
 //Rattachement des balises au DOM
 const sectionFiches = document.querySelector(".fiches");
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(disponibiliteElement);
